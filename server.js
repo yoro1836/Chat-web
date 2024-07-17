a358a61e166928516688fcaffa08cfdd0f75ffa5
yoro1836 <95689076+yoro1836@users.noreply.github.com>
2024-07-17 17:54:01 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('text-update', (data) => {
+    io.emit('text-update', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});

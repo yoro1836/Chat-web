f2b6945038e389afcb2c822c14d1c3762864aedc
yoro1836 <95689076+yoro1836@users.noreply.github.com>
2024-07-17 17:53:02 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,25 @@
-function updateText() {
-    var inputText = document.getElementById("input-text").value;
-    document.getElementById("output").textContent = inputText;
-  }
-  
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('text-update', (data) => {
+    io.emit('text-update', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
